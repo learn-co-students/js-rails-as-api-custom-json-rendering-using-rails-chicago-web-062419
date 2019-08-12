abad65a76a6e5c6ce4d38eee5c7a9359e81a900f
github username <github email address>
2019-08-11 21:04:58 -0500

    Done.

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,18 @@
 class BirdsController < ApplicationController
   def index
-    @birds = Bird.all
-    render json: @birds
+    birds = Bird.all
+    render json: birds, only: [:id, :name, :species]
   end
+
+# We can define our own error messages rather than receive a HTTP error with the conditional statement
+
+  def show 
+    bird = Bird.find_by(id: params[:id])
+    if bird
+      render json: bird, only: [:id, :name, :species]
+    else 
+      render json: {message: 'Bird not found'}
+    end
+  end
+
 end
